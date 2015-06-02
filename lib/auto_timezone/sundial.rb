8c0630aeeef21a3841918d0aea12d4cb03a7a7ec
Smudge <nathan@ngriffith.com>
2015-06-02 12:52:28 -0400

    Honor 'default_timezone' block if it is provided.

--- lib/auto_timezone/sundial.rb
+++ lib/auto_timezone/sundial.rb
@@ -2,6 +2,8 @@ module AutoTimezone
   class Sundial
     def initialize(ctx)
       @ctx = ctx
+      return if !config.default_timezone_block || @ctx.respond_to?(:auto_timezone_default)
+      @ctx.define_singleton_method :auto_timezone_default, config.default_timezone_block
     end
 
     def current_timezone
@@ -9,14 +11,18 @@ module AutoTimezone
     end
 
     def self.preferred_zones
-      @preferred_zones ||= AutoTimezone.config.preferred_timezones.map { |z| Time.find_zone(z) }
+      @preferred_zones ||= config.preferred_timezones.map { |z| Time.find_zone(z) }
     end
 
   private
 
     def timezone_name
-      cookies[:auto_timezone] ||= @ctx.session[:time_zone_name] # TODO: Use configuration for this.
+      cookies[:auto_timezone] ||= timezone_from_config_block
       cookies[:auto_timezone] ||= timezone_from_offset
+    end
+
+    def timezone_from_config_block
+      @ctx.auto_timezone_default if @ctx.respond_to?(:auto_timezone_default)
     end
 
     def timezone_from_offset
@@ -29,6 +35,10 @@ module AutoTimezone
 
     def cookies
       @ctx.send(:cookies)
+    end
+
+    def config
+      AutoTimezone.config
     end
   end
 end
